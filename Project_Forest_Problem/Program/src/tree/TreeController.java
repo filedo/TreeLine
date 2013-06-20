fdbbe337fa58a6bcd480ae7bb3257a161bf1aa13
YuukiYasuda <s_p_0_g_creator-12107-y@ezweb.ne.jp>
2013-06-20 15:55:12 +0900

    TreeControllerの処理追加

--- Project_Forest_Problem/Program/src/tree/TreeController.java
+++ Project_Forest_Problem/Program/src/tree/TreeController.java
@@ -1,5 +1,21 @@
 package tree;
 
-public class TreeController {
+import java.awt.Point;
+import java.awt.event.MouseEvent;
 
+public class TreeController extends mvc.Controller {
+
+		public TreeController()
+		{
+			super();
+		}
+		
+		// クリックした位置座標をLeafに送信する(安田)
+		public void mouseClicked(MouseEvent aMouseEvent)
+		{
+			Point aPoint = aMouseEvent.getPoint();
+			aPoint.translate(view.scrollAmount().x, view.scrollAmount().y);
+			// Leaf.???(aPoint)
+			return;
+		}
 }
