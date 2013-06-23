ffe488a8e1fca29aaff7b25f1edd2887d2f1ad51
YuukiYasuda <s_p_0_g_creator-12107-y@ezweb.ne.jp>
2013-06-24 00:43:23 +0900

    TreeControllerのコメント分追加

--- Project_Forest_Problem/Program/src/tree/TreeController.java
+++ Project_Forest_Problem/Program/src/tree/TreeController.java
@@ -14,7 +14,10 @@ public class TreeController extends mvc.Controller {
 			super();
 		}
 		
-		// ?N???b?N??????u???W??Leaf????M????(???c)
+		/**
+		 * 指定されたマウスイベントからマウスカーサの位置を獲得して、モデル座標系でのクリック位置を割り出して、
+		 * Leafに位置座標の情報を送信する。
+		 */
 		public void mouseClicked(MouseEvent aMouseEvent)
 		{
 			Point aPoint = aMouseEvent.getPoint();
